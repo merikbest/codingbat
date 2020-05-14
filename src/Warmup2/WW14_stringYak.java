ebee852fd478126d998a4d997b476d7b27f33607
merikbest <merikbest2015@gmail.com>
2020-05-14 23:45:36 +0300

    done

---
+++ src/Warmup2/WW14_stringYak.java
@@ -0,0 +1,46 @@
+package Warmup2;
+/*
+Suppose the string "yak" is unlucky.
+ Given a string, return a version where all the "yak" are removed, but the "a" can be any char.
+ The "yak" strings will not overlap.
+
+        stringYak("yakpak") → "pak"
+        stringYak("pakyak") → "pak"
+        stringYak("yak123ya") → "123ya"*/
+
+class StringYak{
+    public String stringYak(String str) {
+
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            int end = i + 3;
+            if (end > str.length())
+                end = str.length();
+
+            if (!str.substring(i, end).equals("yak"))
+                result = str.replace("yak","");
+        }
+        return result;
+
+        // String result = "";
+
+        // for (int i=0; i<str.length(); i++) {
+        //   // Look for i starting a "yak" -- advance i in that case
+        //   if (i+2<str.length() && str.charAt(i)=='y' && str.charAt(i+2)=='k') {
+        //     i =  i + 2;
+        //   } else { // Otherwise do the normal append
+        //     result = result + str.charAt(i);
+        //   }
+        // }
+
+        // return result;
+    }
+}
+
+public class WW14_stringYak {
+    public static void main(String[] args) {
+        StringYak stringYak = new StringYak();
+        System.out.println(stringYak.stringYak("yak123ya"));
+
+    }
+}
